252cae02f3f7cc9bce8c14b8e6553e6ac7d38708
Ricardo Antunes <me@riscadoa.com>
2021-11-14 14:29:27 +0000

    Added Mat3

--- src/app/math.ts
+++ src/app/math.ts
@@ -84,4 +84,121 @@ export class Vec2 {
   public normalize(): Vec2 {
     return this.div(this.length());
   }
+
+  /**
+   * Returns a vector perpendicular to this matrix.
+   * @returns The result. 
+   */
+  public perpendicular(): Vec2 {
+    return new Vec2(-this.y, this.x);
+  }
+}
+
+/**
+ * Implements a 3D matrix, with support for matrix math operations.
+ */
+export class Mat3 {
+  /**
+   * The matrix elements.
+   */
+  public elements: Float32Array;
+
+  /**
+   * @param elements The matrix elements.
+   */
+  constructor(elements: number[] = [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]) {
+    this.elements = new Float32Array(elements);
+  }
+
+  /**
+   * Multiplies this matrix by another matrix and returns the result.
+   * @param other The other matrix.
+   * @returns The result matrix.
+   */
+  public mul(other: Mat3): Mat3;
+
+  /**
+   * Multiplies this matrix by a scalar and returns the result.
+   * @param scalar The scalar.
+   * @returns The result matrix.
+   */
+  public mul(scalar: number): Mat3;
+
+  /**
+   * Multiplies this matrix by a vector and returns the result.
+   * @param v The vector.
+   * @returns The result vector.
+   */
+  public mul(other: Vec2): Vec2;
+
+  // Implementation of the above two overloads.
+  public mul(other: Mat3|number|Vec2): Mat3|Vec2 {
+    if (other instanceof Mat3) {  // Multiply by another matrix.
+      const result = new Mat3();
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          let sum = 0.0;
+          for (let k = 0; k < 3; k++) {
+            sum += this.elements[i * 3 + k] * other.elements[k * 3 + j];
+          }
+          result.elements[i * 3 + j] = sum;
+        }
+      }
+      return result;
+    } else if (other instanceof Vec2) {  // Multiply by vector
+      const result = new Vec2(0.0, 0.0);
+      result.x += this.elements[0 * 3 + 0] * other.x;
+      result.x += this.elements[0 * 3 + 1] * other.y;
+      result.x += this.elements[0 * 3 + 2];
+      result.y += this.elements[0 * 3 + 0] * other.x;
+      result.y += this.elements[0 * 3 + 1] * other.y;
+      result.y += this.elements[0 * 3 + 2];
+      return result;
+    } else {  // Multiply by scalar
+      const result = new Mat3();
+      for (let i = 0; i < 9; i++) {
+        result.elements[i] = this.elements[i] * other;
+      }
+      return result;
+    }
+  }
+
+  /**
+   * Transposes this matrix and returns the result.
+   * @returns The result matrix.
+   */
+  public transpose(): Mat3 {
+    const result = new Mat3();
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        result.elements[i * 3 + j] = this.elements[j * 3 + i];
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Returns the identity matrix.
+   * @returns The identity matrix.
+   */
+  public static identity(): Mat3 {
+    return new Mat3();
+  }
+
+  /**
+   * Returns the translation matrix.
+   * @param v The vector.
+   */
+  public static translation(v: Vec2): Mat3 {
+    return new Mat3([1.0, 0.0, 0.0, 0.0, 1.0, 0.0, v.x, v.y, 1.0]);
+  }
+
+  /**
+   * Returns the scale matrix.
+   * @param v The vector.
+   * @returns The scale matrix.
+   */
+  public static scale(v: Vec2): Mat3 {
+    return new Mat3([v.x, 0.0, 0.0, 0.0, v.y, 0.0, 0.0, 0.0, 1.0]);
+  }
 }
