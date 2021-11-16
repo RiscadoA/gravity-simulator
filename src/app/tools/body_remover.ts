9ef7d689227dc5fe35d6eb269f1e7444d32c745f
Ricardo Antunes <me@riscadoa.com>
2021-11-16 20:09:48 +0000

    Added body remover tool

---
+++ src/app/tools/body_remover.ts
@@ -0,0 +1,46 @@
+import {Vec2} from '../math';
+import {View} from '../renderer';
+import {World} from '../world';
+
+import {Tool} from './tool';
+
+/**
+ * Tool for removing bodies to the world.
+ */
+export class BodyRemover extends Tool {
+  /** World to add bodies to. */
+  private world: World;
+
+  /** View being used to render. */
+  private view: View;
+
+  /**
+   * @param world The world to remove bodies from.
+   * @param view The view being used to render.
+   */
+  constructor(world: World, view: View) {
+    super();
+    this.world = world;
+    this.view = view;
+  }
+
+  public override activate(): void {
+    // Do nothing.
+  }
+
+  public override draw(): void {
+    // Do nothing.
+  }
+
+  public override onMouseDown(_: Vec2): void {
+    // Do nothing.
+  }
+
+  public override onMouseUp(position: Vec2): void {
+    this.world.removeBody(this.view.screenToWorld(position));
+  }
+
+  public override onMouseMove(_: Vec2): void {
+    // Do nothing.
+  }
+}
