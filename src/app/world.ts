4d18902b2df3adf5aa431f987513f4dbe5c788d7
Ricardo Antunes <me@riscadoa.com>
2021-11-16 20:11:23 +0000

    World improvements

--- src/app/world.ts
+++ src/app/world.ts
@@ -19,12 +19,26 @@ export class World {
 
   /**
    * Adds a body to the world.
-   * @param mass The mass of the body.
-   * @param position The initial position of the body.
-   * @param velocity The initial velocity of the body.
+   * @param body Body to add.
    */
-  public addBody(mass: number, position: Vec2, velocity: Vec2): void {
-    this.bodies.push(new Body(mass, position, velocity, Color.random().add(new Color(0.1, 0.1, 0.1))));
+  public addBody(body: Body): void {
+    this.bodies.push(body);
+  }
+
+  /**
+   * Removes a body from the world.
+   * @param position The position of the body to remove.
+   */
+  public removeBody(position: Vec2): void {
+    for (let i = 0; i < this.bodies.length; i++)
+      if (this.bodies[i].intersects(position)) this.bodies.splice(i, 1);
+  }
+
+  /**
+   * Deletes all bodies in the world.
+   */
+  public clear(): void {
+    this.bodies = [];
   }
 
   /**
@@ -44,10 +58,10 @@ export class World {
         const body2 = this.bodies[j];
 
         // Calculate the force of gravity between the two bodies.
-        const offset = body1.getPosition().sub(body2.getPosition());
+        const offset = body1.position.sub(body2.position);
         const sqrDistance = offset.sqrLength();
         const direction = offset.normalize();
-        const force = direction.mul(GRAVITY_CONSTANT * body1.getMass() * body2.getMass() / sqrDistance);
+        const force = direction.mul(GRAVITY_CONSTANT * body1.mass * body2.mass / sqrDistance);
 
         // Apply the force to the bodies.
         this.bodies[j].applyForce(force, dt);
@@ -55,11 +69,10 @@ export class World {
       }
     }
 
-    // Check for collisions between bodies.
     for (let i = 0; i < this.bodies.length; i++) {
       for (let j = i + 1; j < this.bodies.length; j++) {
         // Check for collision.
-        if (this.bodies[i].collides(this.bodies[j])) {
+        if (this.bodies[i].intersects(this.bodies[j])) {
           // Create a new body from the two bodies.
           this.bodies[i] = this.bodies[i].merge(this.bodies[j]);
           // Remove the second body.
@@ -75,7 +88,7 @@ export class World {
    */
   public draw(renderer: Renderer): void {
     for (let i = 0; i < this.bodies.length; i++) {
-      renderer.drawCircle(this.bodies[i].getPosition(), this.bodies[i].getRadius(), this.bodies[i].getColor());
+      renderer.drawCircle(this.bodies[i].position, this.bodies[i].radius, this.bodies[i].color);
     }
   }
 }
