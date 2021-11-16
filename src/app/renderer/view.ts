24f55a9ffcae2b31e453ac663cfc570e106050b5
Ricardo Antunes <me@riscadoa.com>
2021-11-16 20:11:09 +0000

    Improved View

--- src/app/renderer/view.ts
+++ src/app/renderer/view.ts
@@ -11,17 +11,29 @@ export class View {
   private position: Vec2;
 
   /** View scale. */
-  private scale: number;
+  private _scale: number;
 
   /** Aspect ratio. */
   private aspectRatio: number;
 
+  /** Canvas width. */
+  private width: number;
+
+  /** Canvas height. */
+  private height: number;
+
+  /** Zoom change callback. */
+  private onZoomChangeCallback: () => void;
+
   // Default constructor
   constructor(width: number, height: number) {
     this.position = new Vec2(0.0, 0.0);
-    this.scale = 1.0;
+    this._scale = 1.0;
     this.aspectRatio = height / width;
+    this.width = width;
+    this.height = height;
     this.updateTransform();
+    this.onZoomChangeCallback = () => {};
   }
 
   /**
@@ -43,11 +55,18 @@ export class View {
 
   /**
    * Sets the view's scale.
-   * @param scale The new scale.
    */
-  public setScale(scale: number): void {
-    this.scale = scale;
+  public set scale(scale: number) {
+    this._scale = scale;
     this.updateTransform();
+    this.onZoomChangeCallback();
+  }
+
+  /**
+   * Gets the view's scale.
+   */
+  public get scale(): number {
+    return this._scale;
   }
 
   /**
@@ -63,7 +82,27 @@ export class View {
    * @param factor The zoom factor.
    */
   public zoom(multiplier: number): void {
-    this.setScale(this.scale / multiplier);
+    this.scale /= multiplier;
+  }
+
+  /**
+   * Converts from screen coordinates to world coordinates.
+   * @param screen The screen coordinates.
+   * @return The world coordinates.
+   */
+  public screenToWorld(screen: Vec2): Vec2 {
+    let normalized = new Vec2(screen.x / this.width, screen.y / this.height);
+    normalized = normalized.sub(new Vec2(0.5, 0.5)).mul(2.0);
+    normalized.y *= -1.0;
+    return this.transform.inverse().mul(normalized);
+  }
+
+  /**
+   * Sets the zoom change callback.
+   * @param callback The callback.
+   */
+  public setOnZoomChange(callback: () => void): void {
+    this.onZoomChangeCallback = callback;
   }
 
   /**
