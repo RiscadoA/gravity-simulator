2f28f705fc3e02896192641de47974194fe501c2
Ricardo Antunes <me@riscadoa.com>
2021-11-17 16:24:39 +0000

    Added drawLine and drawArrow

---
+++ src/app/renderer/draw_line.ts
@@ -0,0 +1,29 @@
+import {Vec2} from '../math';
+import {Color} from './color';
+
+/**
+ * Represents a command used to draw an line on the screen.
+ */
+export class DrawLine {
+  /** Start of the line. */
+  public start: Vec2;
+  /** End of the line. */
+  public end: Vec2;
+  /** Thickness of the line. */
+  public thickness: number;
+  /** Color of the line. */
+  public color: Color;
+
+  /**
+   * @param start Start position of the line. 
+   * @param end End position of the line.
+   * @param thickness Thickness of the line.
+   * @param color Color of the line.
+   */
+  constructor(start: Vec2, end: Vec2, thickness: number, color: Color) {
+    this.start = start;
+    this.end = end;
+    this.thickness = thickness;
+    this.color = color;
+  }
+}
--- src/app/renderer/index.ts
+++ src/app/renderer/index.ts
@@ -1,18 +1,25 @@
 import {Mat3, Vec2} from '../math';
 
 import {Color} from './color';
+import {DrawLine} from './draw_line';
 import {DrawCircle} from './draw_circle';
 import {View} from './view';
 
 export {View};
 
-type DrawCommand = DrawCircle;
+type DrawCommand = DrawCircle|DrawLine;
 
 /** Background color of the application. */
 const BACKGROUND_COLOR: Color = new Color(0.05, 0.05, 0.05);
 
 /** Number of divisions used for drawing circles. */
 const CIRCLE_DIVISIONS: number = 32;
+
+/** Line thickness scale. */
+const LINE_THICKNESS_SCALE = 0.0025;
+
+/** Arrow head scale. */
+const ARROW_HEAD_SCALE: number = 0.02;
 
 /**
  * Class used to render the app.
@@ -51,8 +58,11 @@ export class Renderer {
   /** Vertex buffer used for drawing. */
   private vertexBuffer: WebGLBuffer;
 
-  /** Circle first vertice and vertex count */
+  /** Circle first vertice and vertex count. */
   private circle: [number, number];
+
+  /** Line first vertice and vertex count. */
+  private line: [number, number];
 
   /**
    * @param canvas Canvas to use.
@@ -83,6 +93,33 @@ export class Renderer {
   }
 
   /**
+   * Draws a line.
+   * @param start The start of the line.
+   * @param end The end of the line.
+   * @param thickness Thickness of the line.
+   * @param color The color of the line.
+   */
+   public drawLine(start: Vec2, end: Vec2, thickness: number, color: Color): void {
+    this.commands.push(new DrawLine(start, end, thickness, color));
+  }
+
+  /**
+   * Draws an arrow.
+   * @param start The start of the arrow.
+   * @param end The end of the arrow.
+   * @param color The color of the arrow.
+   */
+  public drawArrow(start: Vec2, end: Vec2, color: Color): void {
+    const offset = end.sub(start);
+    const perpendicular = offset.perpendicular();
+    const diagonal1 = offset.add(perpendicular).normalize();
+    const diagonal2 = offset.sub(perpendicular).normalize();
+    this.drawLine(start, end, 1.0 / this.view.scale, color);
+    this.drawLine(end, end.add(diagonal1.mul(-ARROW_HEAD_SCALE / this.view.scale)), 1.0 / this.view.scale, color)
+    this.drawLine(end, end.add(diagonal2.mul(-ARROW_HEAD_SCALE / this.view.scale)), 1.0 / this.view.scale, color)
+  }
+
+  /**
    * Flushes the renderer, showing the current state of the app.
    */
   public flush(): void {
@@ -105,6 +142,16 @@ export class Renderer {
         this.context.uniformMatrix3fv(this.transformUniform, false, transform.elements);
         this.context.uniform3f(this.colorUniform, command.color.r, command.color.g, command.color.b);
         this.context.drawArrays(this.context.TRIANGLE_FAN, this.circle[0], this.circle[1]);
+      } else if (command instanceof DrawLine) {
+        const translation = Mat3.translation(command.start);
+        const direction = command.end.sub(command.start);
+        const scale = Mat3.scale(new Vec2(command.thickness * LINE_THICKNESS_SCALE, direction.length()));
+        const rotation = Mat3.rotation(Math.atan2(direction.y, direction.x) - Math.PI / 2);
+        const transform = scale.mul(rotation).mul(translation).mul(this.view.getTransform());
+
+        this.context.uniformMatrix3fv(this.transformUniform, false, transform.elements);
+        this.context.uniform3f(this.colorUniform, command.color.r, command.color.g, command.color.b);
+        this.context.drawArrays(this.context.TRIANGLE_FAN, this.line[0], this.line[1]);
       }
     }
 
@@ -167,6 +214,13 @@ export class Renderer {
       vertices.push(Math.cos(angle), Math.sin(angle));
     }
 
+    // Generate line vertices
+    this.line = [vertices.length / 2, 4];
+    vertices.push(-1.0, 0.0);
+    vertices.push(-1.0, 1.0);
+    vertices.push(+1.0, 1.0);
+    vertices.push(+1.0, 0.0);
+    
     // Generate vertex buffer
     this.vertexBuffer = this.context.createBuffer()!;
     this.context.bindBuffer(this.context.ARRAY_BUFFER, this.vertexBuffer);
